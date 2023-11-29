32da92de9ce2fdf8610e77ae09341806647826fe
moon <lmj970302@gmail.com>
2023-11-29 18:22:21 +0900

    🌱 Combine_Subject_PassthroughSubject, CurrentSubject 사용해보기

--- Combine/Combine_1.playground/Pages/Subject.xcplaygroundpage/Contents.swift
+++ Combine/Combine_1.playground/Pages/Subject.xcplaygroundpage/Contents.swift
@@ -2,13 +2,33 @@ import Foundation
 import Combine
 
 // PassthroughSubject
+let relay = PassthroughSubject<String,Never>()
+let subscription1 = relay.sink { value in
+    print("subscription1 recived value: \(value)")
+}
+relay.send("Hello")
+relay.send("World")
+    
+
+
+// CurrentValueSubject : output에 대한 초기값을 주어야 한다.
+let variable = CurrentValueSubject<String, Never>("")
+
+// subscription 하기 전에 초기 값을 줄 수 있다.
+variable.send("Initial text")
+
+let subscription2 = variable.sink { value in
+    print("subscription2 recived value: \(value)")
+}
+
+variable.send("More text")
+
+// value로 출력된 데이터를 확인할 수 있다.
+variable.value
 
 
 
 
-// CurrentValueSubject
-
-
-
-
-
+let publicher = ["Here", "we", "go"].publisher
+publicher.subscribe(relay)
+publicher.subscribe(variable)
