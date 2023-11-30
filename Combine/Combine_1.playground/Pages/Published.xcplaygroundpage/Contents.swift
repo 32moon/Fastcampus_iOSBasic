4b0b5d93590f3711c6523fc0e1296db26088cce4
moon <lmj970302@gmail.com>
2023-11-30 16:01:59 +0900

    🌱 @Published 사용해보기

--- Combine/Combine_1.playground/Pages/Published.xcplaygroundpage/Contents.swift
+++ Combine/Combine_1.playground/Pages/Published.xcplaygroundpage/Contents.swift
@@ -4,6 +4,24 @@ import Foundation
 import UIKit
 import Combine
 
+final class SomeViewModel {
+    @Published var name: String = "Jack"
+    var age: Int = 20
+}
+
+final class Label {
+    var text: String = ""
+}
+
+let label = Label()
+let viewModel = SomeViewModel()
+print("--> text: \(label.text)")
+
+viewModel.$name.assign(to: \.text, on: label)
+print("--> assign text: \(label.text)")
+
+viewModel.name = "Jason"
+print("--> update text: \(label.text)")
 
 
 
