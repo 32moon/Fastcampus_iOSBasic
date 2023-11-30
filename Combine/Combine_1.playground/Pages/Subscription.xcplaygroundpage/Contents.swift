910ae4a88ae36c240c43cd6b627ac8a2da619693
moon <lmj970302@gmail.com>
2023-11-30 15:52:53 +0900

    ✨ Subscription 디버그, cancel, send finished 사용해보기

--- Combine/Combine_1.playground/Pages/Subscription.xcplaygroundpage/Contents.swift
+++ Combine/Combine_1.playground/Pages/Subscription.xcplaygroundpage/Contents.swift
@@ -4,9 +4,25 @@ import Foundation
 import Combine
 
 
+let subject = PassthroughSubject<String, Never>()
 
 
 // The print() operator prints you all lifecycle events
+let subscription = subject
+    .print("[--> 디버그]") // 디버그 가능
+    .sink { value in
+    print("subscriber recieved value: \(value)")
+}
 
+subject.send("Hello")
+subject.send("Hello again")
+subject.send("Hello for the last time!")
+
+// 데이터 전달 종료
+subject.send(completion: .finished)
+// 구독 관계 취소
+subscription.cancel()
+
+subject.send("Hello ??")
 
 //: [Next](@next)
