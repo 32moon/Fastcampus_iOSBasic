21315f80789603c55c232fcc33648311186b2239
moon <lmj970302@gmail.com>
2023-12-04 18:14:10 +0900

    🌱operator_ Map, Filter 사용해보기

--- Combine/Combine_1.playground/Pages/Operator - map & filter.xcplaygroundpage/Contents.swift
+++ Combine/Combine_1.playground/Pages/Operator - map & filter.xcplaygroundpage/Contents.swift
@@ -4,9 +4,31 @@ import Foundation
 import Combine
 
 // Transform - Map
+let numPublisher = PassthroughSubject<Int, Never>()
+let subscruption1 = numPublisher
+    .map { $0 * 2 }
+    .sink { value in
+        print("Transfored Value: \(value)")
+    }
+
+numPublisher.send(10)
+numPublisher.send(20)
+numPublisher.send(30)
+subscruption1.cancel()
 
 
 // Filter
+let stringPublisher = PassthroughSubject<String, Never>()
+let subscription2 = stringPublisher
+    .filter { $0.contains("a") }
+    .sink { value in
+        print("Filter Value: \(value)")
+    }
+
+stringPublisher.send("abc")
+stringPublisher.send("jack")
+stringPublisher.send("joon")
+subscription2.cancel()
 
 
 //: [Next](@next)
