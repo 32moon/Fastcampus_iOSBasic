128dc36dc4b853745d6dcb249fe1d79be500dbd0
moon <lmj970302@gmail.com>
2023-12-04 18:06:53 +0900

    🌱 Scheduler 사용해보기

--- Combine/Combine_1.playground/Pages/Scheduler.xcplaygroundpage/Contents.swift
+++ Combine/Combine_1.playground/Pages/Scheduler.xcplaygroundpage/Contents.swift
@@ -3,8 +3,21 @@
 import Foundation
 import Combine
 
+// 어떤 스레드에서 작업을 할 건지, 작업 처리 우선 순위를 정하여 지정해 줄 수 있다.
+let arrayPublisher = [1, 2, 3].publisher
 
+let queue = DispatchQueue(label: "custom")
 
+let subscription = arrayPublisher
+    .subscribe(on: queue)
+    .map { value -> Int in
+        print("transform: \(value), thread: \(Thread.current)")
+        return value
+    }
+    .receive(on: DispatchQueue.main)
+    .sink { value in
+    print("Receive Value: \(value), thread: \(Thread.current)")
+}
 
 
 //: [Next](@next)
