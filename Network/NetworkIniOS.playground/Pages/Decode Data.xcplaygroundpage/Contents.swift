d52c21621ef2db1109c2e3f8f3aa9aa580a0e8e1
moon <lmj970302@gmail.com>
2023-12-11 17:00:35 +0900

    ✨ Network_iOS_DataDecoding

--- Network/NetworkIniOS.playground/Pages/Decode Data.xcplaygroundpage/Contents.swift
+++ Network/NetworkIniOS.playground/Pages/Decode Data.xcplaygroundpage/Contents.swift
@@ -2,6 +2,44 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+struct GithubProfile: Codable {
+    let login: String
+    let avatarUrl: String
+    let htmlUrl: String
+    let followers: Int
+    let following: Int
+    
+    enum CodingKeys: String, CodingKey {
+        // JOSN 데이터 키값 
+        case login
+        case avatarUrl = "avatar_url"
+        case htmlUrl = "html_url"
+        case followers
+        case following
+    }
+}
 
-//: [Next](@next)
+let configuration = URLSessionConfiguration.default
+let session = URLSession(configuration: configuration)
+
+let url = URL(string: "http://api.github.com/users/32moon")!
+
+let task = session.dataTask(with: url) { data, response, error in
+    
+    guard let httpResponse = response as? HTTPURLResponse,
+          (200..<300).contains(httpResponse.statusCode) else {
+        print ("---> response \(response)")
+        return
+    }
+    
+    guard let data = data else { return }
+    do {
+        let decoder = JSONDecoder()
+        let profile = try decoder.decode(GithubProfile.self, from: data)
+        print("profile: \(profile)")
+    } catch let error as NSError {
+        print("error : \(error)")
+    }
+}
+
+task.resume()
